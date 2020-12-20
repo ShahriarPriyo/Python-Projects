e81b056f5d5ba2d7247064b2269d3919791bf71d
Shahriar Priyo <46930074+ShahriarPriyo@users.noreply.github.com>
2020-12-20 10:37:57 +0600

    Add files via upload

---
+++ Madlib_game/madlib.py
@@ -0,0 +1,12 @@
+#madlib project 
+adj=input("Adjective 1:")
+adj2=input("Adjective 2:")
+verb=input("Verb:")
+adj3=input("Adjective 3:")
+
+
+ml=f"Computer programming is very git init{adj}.It is one of the most effective way to show your \
+{adj2}.So, {verb} is not easy to earn.You have to work real {adj3} to master the art \
+of computer programming."
+
+print(ml)
